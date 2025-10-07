b621840d73c069f647279d228469db958e6dfaec
NatnaelAyele <n.eticha@alustudent.com>
2025-10-07 14:08:48 +0200

    a shell script code that provides a menu to choose which log file to analyze, accepts input from the user, and analyzes the chosen file accordingly

---
+++ hospital_data/anaalyze_logs.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+heart_rate_analyzer () {
+	log_file=Coding-lab_Group2/hospital_data/active_logs/heart_rate.log
+	report_file=Coding-lab_Group2/hospital_data/reports/analysis_report.txt
+	if [ ! -f "$log_file" ]; then
+		echo "log file not found"
+	else 
+		echo "Report for heart log:" >> $report_file
+		cut -d' ' -f3 "$log_file" | sort | uniq -c | awk '{print $2 " has" ": " $1 " entries"}' >> "$report_file"
+		echo >> $report_file
+		echo "Time stamp of the first entry: ">> $report_file
+                head -n 1 "$log_file" | awk '{print $1, $2}' >> $report_file
+                echo >> $report_file
+                echo "Time stamp of the last entry: ">> $report_file
+                tail -n 1 "$log_file" | awk '{print $1, $2}' >> $report_file
+	fi
+}
+
+temp_analyzer () {
+	log_file=Coding-lab_Group2/hospital_data/active_logs/temperature.log
+	report_file=Coding-lab_Group2/hospital_data/reports/analysis_report.txt
+        if [ ! -f $log_file ]; then
+                echo "log file not found"
+        else
+                echo "Report for temperature log:" >> $report_file
+                cut -d' ' -f3 "$log_file" | sort | uniq -c | awk '{print $2 " has" ": " $1 " entries"}' >> "$report_file"
+                echo >> $report_file
+		echo "Time stamp of the first entry: ">> $report_file
+                head -n 1 "$log_file" | awk '{print $1, $2}' >> $report_file
+		echo >> $report_file
+		echo "Time stamp of the last entry: ">> $report_file
+                tail -n 1 "$log_file" | awk '{print $1, $2}' >> $report_file
+	fi
+}
+
+water_usage_analyzer () {
+        log_file=Coding-lab_Group2/hospital_data/active_logs/water_usage.log
+	report_file=Coding-lab_Group2/hospital_data/reports/analysis_report.txt
+        if [ ! -f $log_file ]; then
+                echo "log file not found"
+        else
+                echo "Report for water usage log:" >> $report_file
+                cut -d' ' -f3 "$log_file" | sort | uniq -c | awk '{print $2 " has" ": " $1 " entries"}' >> "$report_file"
+                echo >> $report_file
+                echo "Time stamp of the first entry: ">> $report_file
+                head -n 1 "$log_file" | awk '{print $1, $2}' >> $report_file
+                echo >> $report_file
+                echo "Time stamp of the last entry: ">> $report_file
+                tail -n 1 "$log_file" | awk '{print $1, $2}' >> $report_file
+        fi
+}
+while true; do
+	echo "==================Menu================"
+	echo "Select log file to analyze: "
+	echo "1) Heart Rate (heart_rate.log)"
+	echo "2) Temperature (temperature.log)"
+	echo "3) Water Usage (water_usage.log)"
+	echo "4) Exit"
+	echo "======================================"
+	read -p "Enter your choice: " choice
+	
+	case $choice in
+		1) heart_rate_analyzer;;
+		2) temp_analyzer;;
+		3) water_usage_analyzer;;
+		4)
+			echo "Exiting......, Good bye"
+			break;;
+		*) 
+			echo "Please enter a number between 1-3"
+	esac
+done
