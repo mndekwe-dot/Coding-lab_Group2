3da695b4b69bfeb2760eb3a3e6b88939a3380bc9
mndekwe-dot <mndekwe@alustudent.com>
2025-10-09 12:18:15 -0700

    fixing bugs

--- hospital_data/archive_logs.sh
+++ hospital_data/archive_logs.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-
+# code below prints text in which the user will have to choose a monitor from.
 echo "Select log to archive:"
 echo "1) Heart Rate"
 echo "2) Temperature"
 echo "3) Water Usage"
 
-read -p "Enter choice (1-3): " choice
+read -p "Enter choice (1-3): " choice # This code  reads the user input choice.
 
-case $choice in
+# this reads the user input and returns the archive_dir corresponding to what choice is made,and if no choice is made, it returns an invalid choice and exits 
+
+case $choice in 
     1)
-        log_file="hospital_data/active_logs/heart_rate.log"
-        archive_dir="hospital_data/heart_data_archive"
+        log_file="active_logs/heart_rate.log"
+        archive_dir="archived_logs/heart_data_archive"
         ;;
     2)
-        log_file="hospital_data/active_logs/temperature.log"
-        archive_dir="hospital_data/temperature_data_archive"
+        log_file="active_logs/temperature.log"
+        archive_dir="archived_logs/temperature_data_archive"
         ;;
     3)
-        log_file="hospital_data/active_logs/water_usage.log"
-        archive_dir="hospital_data/water_usage_archive"
+        log_file="active_logs/water_usage.log"
+        archive_dir="archived_logs/water_usage_archive"
         ;;
     *)
         echo "Invalid choice!"
@@ -26,18 +28,21 @@ case $choice in
         ;;
 esac
 
+#if statement checks if no log  found and exit
+
 if [ ! -f "$log_file" ]; then
     echo "Log file $log_file not found!"
     exit 1
 fi
+#if statement check if no directory found and if not, it create new one.
 
 if [ ! -d "$archive_dir" ]; then
     echo "Archive directory $archive_dir does not exist. Creating it..."
     mkdir -p "$archive_dir"
 fi
 
-timestamp=$(date +%F_%H-%M-%S)
-base_name=$(basename "$log_file" .log)
+timestamp=$(date +%F_%H-%M-%S) #this specify format of time we want
+base_name=$(basename "$log_file" .log) 
 mv "$log_file" "$archive_dir/${base_name}_${timestamp}.log"
 touch "$log_file"
 
